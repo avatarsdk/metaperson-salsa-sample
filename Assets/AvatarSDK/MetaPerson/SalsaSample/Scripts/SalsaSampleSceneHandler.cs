8d70f0ff40ae3c4334779c5634a34bef434b2f0e
nsdfxela nsdfxela <alexander.fedosin@itseez3d.com>
2024-09-09 14:31:02 +0300

    cosmetics

--- Assets/AvatarSDK/MetaPerson/SalsaSample/Scripts/SalsaSampleSceneHandler.cs
+++ Assets/AvatarSDK/MetaPerson/SalsaSample/Scripts/SalsaSampleSceneHandler.cs
@@ -15,7 +15,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
